83da0b86a049bf3cfd02c1d03d4e9ee7deca6e4d
iulusoy <inga.ulusoy@uni-heidelberg.de>
2022-09-13 21:43:48 +0200

    multiple input

--- moralization/inout.py
+++ moralization/inout.py
@@ -1,3 +1,4 @@
+import re
 from cassis import load_typesystem, load_cas_from_xmi
 from collections import defaultdict
 import glob
@@ -9,25 +10,48 @@ pkg = importlib_resources.files("moralization")
 
 class InputOutput:
     def __init__(self) -> None:
-        pass
+        None
 
     @staticmethod
-    def get_input(filename: str) -> object:
-        """Read in the input file. Currently only xmi file format."""
-        # this dict can be extended to contain more file formats
-        input_type = {"xmi": load_cas_from_xmi}
-        file_ending = filename.strip().split(".")[-1]
+    def read_typesystem() -> object:
         # read in the file system types
         file = pkg / "data" / "TypeSystem.xml"
         with open(file, "rb") as f:
             ts = load_typesystem(f)
+        return ts
+
+    @staticmethod
+    def get_input_file(filename: str) -> object:
+        """Read in the input file. Currently only xmi file format."""
+        # this dict can be extended to contain more file formats
+        input_type = {"xmi": load_cas_from_xmi}
+        file_ending = filename.strip().split(".")[-1]
+        ts = InputOutput.read_typesystem()
         # read the actual data file
         with open(filename, "rb") as f:
             data = input_type[file_ending](f, typesystem=ts)
         return data
 
+    @staticmethod
+    def get_input_dir(dir_path: str) -> list:
+        "Get a list of input files from a given directory. Only xmi files."
+        ### load multiple files into a list of dictionaries
+        ts = InputOutput.read_typesystem()
+        data_files = glob.glob(os.path.join(dir_path, "*.xmi"))
+        data_dict_list = {}
+        for data_file in data_files:
+            # the wikipediadiskussionen file breaks as it has an invalid xmi charakter.
+            # if data_file != "../data/Wikipediadiskussionen-neg-BD-neu-optimiert-CK.xmi":
+            with open(data_file, "rb") as f:
+                cas = load_cas_from_xmi(f, typesystem=ts)
+            data_dict_list[os.path.basename(data_file).split(".xmi")[0]] = {
+                # "data": sort_spans(cas, ts),
+                "file_type": os.path.basename(data_file).split(".")[1],
+            }
+        return data_dict_list
+
 
 if __name__ == "__main__":
-    data = InputOutput.get_input(
+    data = InputOutput.get_input_file(
         "moralization/data/Gerichtsurteile-pos-AW-neu-optimiert-BB.xmi"
     )
