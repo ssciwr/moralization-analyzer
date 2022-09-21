0582a4537920d7cc6ee766e1bf55930e80bd6917
GwydionJon <gwy.daskalakis@googlemail.com>
2022-09-21 12:29:19 +0200

    added custom typesystem option

--- moralization/inout.py
+++ moralization/inout.py
@@ -4,6 +4,8 @@ import importlib_resources
 import logging
 from moralization import analyse
 from lxml.etree import XMLSyntaxError
+import glob
+import os
 
 pkg = importlib_resources.files("moralization")
 
@@ -19,10 +21,11 @@ class InputOutput:
         return pathlib.Path(filename).suffix[1:]
 
     @staticmethod
-    def read_typesystem() -> object:
+    def read_typesystem(filename=None) -> object:
+        if filename is None:
+            filename = pkg / "data" / "TypeSystem.xml"
         # read in the file system types
-        myfile = pkg / "data" / "TypeSystem.xml"
-        with open(myfile, "rb") as f:
+        with open(filename, "rb") as f:
             ts = load_typesystem(f)
         return ts
 
@@ -37,16 +40,29 @@ class InputOutput:
         return data
 
     @staticmethod
-    def get_input_dir(dir: str) -> dict:
+    def get_input_dir(dir: str, use_custom_ts=False) -> dict:
         "Get a list of input files from a given directory. Currently only xmi files."
         ### load multiple files into a list of dictionaries
-        ts = InputOutput.read_typesystem()
         dir_path = pathlib.Path(dir)
         if not dir_path.is_dir():
             raise RuntimeError(f"Path {dir_path} does not exist")
         data_files = dir_path.glob("*.xmi")
         if not data_files:
             raise RuntimeError(f"No input files found in {dir_path}")
+
+        if use_custom_ts is False:
+            ts = InputOutput.read_typesystem()
+        else:
+            ts_file = glob.glob(os.path.join(dir_path, "TypeSystem.xml"))
+            if len(ts_file) > 1:
+                raise Warning("Multiple typesystems found. Please provide only one.")
+            elif len(ts_file) == 0:
+                raise FileNotFoundError(
+                    f"Trying to find custom typesystem, but no 'TypeSystem.xml' found in {dir_path}"
+                )
+            else:
+                ts = InputOutput.read_typesystem(ts_file[0])
+
         data_dict = {}
         for data_file in data_files:
             # get the file type dynamically
