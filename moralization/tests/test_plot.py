07e317126407fa2e112dcd544fe19a1450b4d5f3
GwydionJon <gwy.daskalakis@googlemail.com>
2022-11-09 15:48:48 +0100

    added small test_plot

--- moralization/tests/test_plot.py
+++ moralization/tests/test_plot.py
@@ -1,5 +1,5 @@
 from moralization import analyse, plot
-
+import matplotlib
 
 def test_PlotSpans_report_occurrence_heatmap(data_dict):
     df_sentence_occurrence = analyse.AnalyseSpans.report_occurrence_per_paragraph(
@@ -31,3 +31,19 @@ def test_PlotSpans_report_occurrence_matrix(data_dict):
         df_sentence_occurrence,
         filter_=["KAT1-Moralisierendes Segment", "Neutral", "Care"],
     )
+
+
+def test_InteractiveCategoryPlot(data_dict):
+    # removes automatic window opening by matplotlib
+    matplotlib.use("Agg")
+
+    heatmap = plot.InteractiveCategoryPlot(data_dict, figsize=(15, 10))
+    heatmap.show()
+
+    heatmap = plot.InteractiveCategoryPlot(data_dict)
+    heatmap.show()
+
+    heatmap = plot.InteractiveCategoryPlot(
+        data_dict, plot_callback=plot.percent_matrix_heatmap
+    )
+    heatmap.show()
