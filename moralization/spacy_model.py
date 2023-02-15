a55c9a72afcc31f434599e7a98e5f22c60baabab
Inga Ulusoy <inga.ulusoy@uni-heidelberg.de>
2023-02-15 03:23:46 -0800

    Spacy model task 1 (#59)
    * refactored _convert_dict_to_doc_dict to allow different span categories
    
    * updated SpacySetup to include multiple span categories
    
    * updated base config
    
    * added ents to test_model_with_string
    
    * added spacy model to CI instance
    
    * improved support for visualizing multiple files and streamlined setup process
    
    * updated tests to new SpacySetup
    
    * added comments to spacy_demo notebook
    
    * changed to new way of importing collections.abc.Iterable
    
    * added test_model_with_string to tests
    
    * updated test_with_string function to use spacy visualization
    
    * updated test
    
    * added sentence test to notebook
    
    * added option argument to test_model_with_string
    
    * refactored visualize_data
    
    * increased coverage of visualize_data
    
    * fixed wrong filename after merge
    
    ---------
    
    Co-authored-by: GwydionJon <gwy.daskalakis@googlemail.com>

--- moralization/spacy_model.py
+++ moralization/spacy_model.py
@@ -12,6 +12,8 @@ from pathlib import Path
 from spacy.cli.init_config import fill_config
 from sklearn.model_selection import train_test_split
 from tempfile import mkdtemp
+from collections import defaultdict
+from collections.abc import Iterable
 
 
 class SpacySetup:
@@ -29,6 +31,7 @@ class SpacySetup:
 
         """
         self.data_dir, self.working_dir = self._setup_working_dir(data_dir, working_dir)
+        self.convert_data_to_spacy_doc()
 
     def _setup_working_dir(self, data_dir, working_dir):
         """Check if given path is possible and initialize directory if not already present.
@@ -57,9 +60,19 @@ class SpacySetup:
         """Convert the given xmi/xml files to a spacy specific binary filesystem."""
         data_dict = InputOutput.read_data(self.data_dir)
 
-        self.doc_dict = self._convert_dict_to_doc_dict(data_dict)
+        merging_dict = {
+            "sc": "all",
+            "task1": ["KAT1MoralisierendesSegment"],
+            "task2": ["Moralwerte", "KAT2Subjektive_Ausdrcke"],
+            "task3": ["Protagonistinnen", "Protagonistinnen2", "Protagonistinnen3"],
+            "task4": ["KommunikativeFunktion"],
+            "task5": ["Forderung"],
+        }
 
-        return self.doc_dict
+        data_dict = self._add_dict_cat(data_dict, merging_dict)
+        spans_dict = self._convert_dict_to_spans_dict(data_dict)
+
+        self.doc_dict = self._convert_spans_dict_to_doc_dict(data_dict, spans_dict)
 
     def export_training_testing_data(self, output_dir=None):
         """Convert a list of spacy docs to a serialisable DocBin object and save it to disk.
@@ -73,45 +86,121 @@ class SpacySetup:
         """
         if output_dir is None:
             output_dir = self.working_dir
+        elif isinstance(output_dir, str):
+            output_dir = Path(output_dir)
 
         db_train = DocBin()
         db_dev = DocBin()
 
         for file in self.doc_dict.values():
             db_train.add(file["train"])
-            db_dev.add(file["dev"])
+            db_dev.add(file["test"])
 
         db_train.to_disk(output_dir / "train.spacy")
         db_dev.to_disk(output_dir / "dev.spacy")
 
-    def visualize_data(self, filename=None, display_type="all", style="span"):
+    def _manage_visualisation_filenames(self, filenames):
+
+        # check through given filenames and convert ints to key string
+        if filenames is None:
+            filename = list(self.doc_dict.keys())
+        elif isinstance(filenames, str):
+            filename = [filenames]
+        elif isinstance(filenames, int):
+            filename = [list(self.doc_dict.keys())[filenames]]
+        elif isinstance(filenames, Iterable):
+            filename = []
+            for file in filenames:
+                if isinstance(file, int):
+                    filename.append(list(self.doc_dict.keys())[file])
+                else:
+                    filename.append(file)
+
+        # check if all new filenames are in doc_dict.keys()
+        for file in filename:
+            if file not in list(self.doc_dict.keys()):
+                raise IndexError(
+                    f"The filename {file} is not provided in the dataset, which only has {list(self.doc_dict.keys())}."
+                )
+
+        return filename
+
+    def visualize_data(self, filenames=None, type="all", style="span", spans_key="sc"):
         """Use the displacy class offered by spacy to visualize the current dataset.
+            use SpacySetup.span_keys to show possible keys or use 'sc' for all.
+
 
         Args:
           filename(str/list, optional, optional): Specify which of the loaded files should be presented,
           if None all files are shown.
           This can also take a list., defaults to None
           display_type(str, optional, optional): Specify is only the trainings,
-          the testing or all datapoints should be shown, defaults to "all"
+          the testing or all datapoints should be shown,options are: "all", "test" and "train". Defaults to "all"
           type: the visualization type given to displacy, available are "dep", "ent" and "span,
           defaults to "span".
           style:  (Default value = "span")
 
         Returns:
-
+            Displacy.render
         """
+
+        if isinstance(spans_key, list):
+            raise NotImplementedError(
+                "spacy does no support viewing multiple categories at once."
+            )
+            # we could manually add multiple categories to one span cat and display this new category.
+
+        if spans_key != "sc" and spans_key not in self.span_keys:
+            raise ValueError(
+                f"""The provided key: {spans_key} is not valid.
+                Please use one of the following {set(self.span_keys.keys())}"""
+            )
+        if type not in ["all", "test", "train"]:
+            raise IndexError(
+                f"Type argument must be either 'all', 'test' or 'train', but is {type}"
+            )
+
+        filename = self._manage_visualisation_filenames(filenames)
+
         if not is_interactive():
             raise NotImplementedError(
                 "Please only use this function in a jupyter notebook for the time being."
             )
 
-        if filename is None:
-            filename = list(self.doc_dict.keys())
         return displacy.render(
-            [self.doc_dict[file][display_type] for file in filename], style=style
+            [self.doc_dict[file][type] for file in filename],
+            style=style,
+            options={"spans_key": spans_key},
         )
 
-    def _convert_dict_to_doc_dict(self, data_dict):
+    def _add_dict_cat(self, data_dict, new_dict_cat):
+        """Take the new_dict_cat dict and add its key as a main_cat to data_dict.
+        The values are the total sub_dict_entries of the given list.
+
+        Args:
+          data_dict(dict: dict): The datadict generated from xmi files.
+          new_dict_cat(dict): map new category to list of existing_categories.
+
+        Return:
+            dict: The data_dict with new span categories.
+        """
+
+        for file in data_dict.keys():
+            for new_main_cat, new_cat_entries in new_dict_cat.items():
+                if new_cat_entries == "all":
+                    for main_cat in list(data_dict[file]["data"].keys()):
+                        data_dict[file]["data"][new_main_cat].update(
+                            data_dict[file]["data"][main_cat]
+                        )
+                else:
+                    for old_main_cat in new_cat_entries:
+                        data_dict[file]["data"][new_main_cat].update(
+                            data_dict[file]["data"][old_main_cat]
+                        )
+
+        return data_dict
+
+    def _convert_dict_to_spans_dict(self, data_dict):
         """Custom tranformation steps to convert our data_dict into a usable spacy.doc format.
         For this all annotations will be saved under the span_key 'sc'.
 
@@ -122,37 +211,63 @@ class SpacySetup:
           dict: Dict of spacy.doc objects
 
         """
+        self.span_keys = defaultdict(set)
         nlp = spacy.blank("de")
-        doc_dict = {}
+        # defaultdict with structure:
+        # file - main_cat - all/train/test
+        spans_dict = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
 
         for file in data_dict.keys():
-            doc_train = nlp(data_dict[file]["sofa"])
-            doc_dev = nlp(data_dict[file]["sofa"])
-            doc_all = nlp(data_dict[file]["sofa"])
+            # sort out unused categories
+            data_dict[file]["data"].pop("KAT5Ausformulierung", None)
+            for main_cat_value in data_dict[file]["data"].values():
+                main_cat_value.pop("Dopplung", None)
 
-            ents = []
+            doc = nlp(data_dict[file]["sofa"])
 
             for main_cat_key, main_cat_value in data_dict[file]["data"].items():
-                if main_cat_key != "KAT5Ausformulierung":
-                    for sub_cat_label, sub_cat_span_list in main_cat_value.items():
-                        if sub_cat_label != "Dopplung":
-                            for span in sub_cat_span_list:
-                                spacy_span = doc_train.char_span(
-                                    span["begin"],
-                                    span["end"],
-                                    label=sub_cat_label,
-                                )
-                                ents.append(spacy_span)
+                for sub_cat_label, sub_cat_span_list in main_cat_value.items():
+                    self.span_keys[main_cat_key].add(sub_cat_label)
+                    for span in sub_cat_span_list:
+                        spans_dict[file][main_cat_key]["all"].append(
+                            doc.char_span(
+                                span["begin"],
+                                span["end"],
+                                label=sub_cat_label,
+                            )
+                        )
+                if len(spans_dict[file][main_cat_key]["all"]) > 1:
+                    spans_train, spans_test = train_test_split(
+                        spans_dict[file][main_cat_key]["all"],
+                        test_size=0.2,
+                        random_state=42,
+                    )
+                    (
+                        spans_dict[file][main_cat_key]["train"],
+                        spans_dict[file][main_cat_key]["test"],
+                    ) = (spans_train, spans_test)
+                else:
+                    spans_dict[file][main_cat_key]["train"] = spans_dict[file][
+                        main_cat_key
+                    ]["all"]
+            spans_dict[file]["doc"] = doc
 
-            ents_train, ents_test = train_test_split(
-                ents, test_size=0.2, random_state=42
-            )
+        return spans_dict
 
-            doc_train.spans["sc"] = ents_train
-            doc_dev.spans["sc"] = ents_test
-            doc_all.spans["sc"] = ents
+    def _convert_spans_dict_to_doc_dict(self, data_dict, spans_dict):
 
-            doc_dict[file] = {"train": doc_train, "dev": doc_dev, "all": doc_all}
+        doc_dict = {}
+        for file in data_dict.keys():
+            doc = spans_dict[file].pop("doc", None)
+            doc_test = doc.copy()
+            doc_train = doc.copy()
+            doc_dict[file] = {"all": doc, "test": doc_test, "train": doc_train}
+            # each value now has the keys "all"/"train"/test, each with a list of spans
+            for main_cat_key, main_cat_values in spans_dict[file].items():
+                for usecase in ["all", "train", "test"]:
+                    doc_dict[file][usecase].spans[main_cat_key] = main_cat_values[
+                        usecase
+                    ]
 
         return doc_dict
 
@@ -233,7 +348,6 @@ class SpacyTraining:
 
         if config_file:
             config_file = Path(config_file)
-
             if config_file.is_file():
                 config_file = Path(config_file)
             else:
@@ -241,7 +355,8 @@ class SpacyTraining:
                     config_file = self.working_dir / config_file
                 else:
                     raise FileNotFoundError(
-                        f"The given config file could not be found in the working directory: {self.working_dir}"
+                        "The given config file could not be found in the working directory:"
+                        + f" {self.working_dir} or under {config_file.absolute()}"
                     )
 
         else:
@@ -316,7 +431,7 @@ class SpacyTraining:
         )
         return evaluation_data
 
-    def test_model_with_string(self, test_string):
+    def test_model_with_string(self, test_string, options=None):
         """
 
         Args:
@@ -325,10 +440,20 @@ class SpacyTraining:
         Returns:
 
         """
+        if not is_interactive():
+            raise NotImplementedError(
+                "Please only use this function in a jupyter notebook for the time being."
+            )
+        if options is None:
+            options = {"spans_key": "task1"}
+
         nlp = spacy.load(self._best_model())
         doc = nlp(test_string)
-        for span in doc.spans["sc"]:
-            print(span, span.label_)
+
+        displacy.render(doc, style="span", options=options)
+        displacy.render(doc, style="ent")
+
+        return doc, nlp
 
     def _best_model(self):
         """ """
