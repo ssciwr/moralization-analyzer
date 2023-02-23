6eafa5156a81b8b83f44ae59985f6ea313abdc00
GwydionJon <gwy.daskalakis@googlemail.com>
2023-02-23 15:34:16 +0100

    updated ini to only have the datamanager

--- moralization/__init__.py
+++ moralization/__init__.py
@@ -1,21 +1,9 @@
 import importlib_metadata as metadata
-from moralization.input_data import InputOutput
-
-# from moralization.analyse import AnalyseOccurrence, AnalyseSpans
-# from moralization.plot import PlotSpans, InteractiveCategoryPlot
-# from moralization.spacy_model import SpacySetup, SpacyTraining
+from moralization.data_manager import DataManager
 
 # Export the version defined in project metadata
 __version__ = metadata.version(__package__)
 del metadata
 
 
-__all__ = [
-    "InputOutput",
-    "AnalyseOccurrence",
-    "AnalyseSpans",
-    "PlotSpans",
-    "InteractiveCategoryPlot",
-    "SpacySetup",
-    "SpacyTraining",
-]
+__all__ = [DataManager]
