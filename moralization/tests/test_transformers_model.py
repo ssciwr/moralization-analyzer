28f1733555dd232bff28f6abee119dc6935417bb
Inga Ulusoy <inga.ulusoy@uni-heidelberg.de>
2023-04-17 13:29:56 +0200

    fix error type

--- moralization/tests/test_transformers_model.py
+++ moralization/tests/test_transformers_model.py
@@ -93,5 +93,5 @@ def test_init_model():
     test_obj = TransformersSetup()
     test_obj.init_model()
     assert test_obj.tokenizer.is_fast
-    with pytest.raises(ValueError):
+    with pytest.raises(OSError):
         test_obj.init_model(model_name="Testing")
