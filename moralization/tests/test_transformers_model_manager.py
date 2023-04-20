ae4864687402a86034dd0508d43b9c369d5b8a5f
Inga Ulusoy <inga.ulusoy@uni-heidelberg.de>
2023-04-20 13:50:58 +0200

    clean-up 1

--- moralization/tests/test_transformers_model_manager.py
+++
@@ -1,6 +0,0 @@
-def test_init_model():
-    test_obj = TransformersSetup()
-    test_obj.init_model()
-    assert test_obj.tokenizer.is_fast
-    with pytest.raises(OSError):
-        test_obj.init_model(model_name="Testing")
